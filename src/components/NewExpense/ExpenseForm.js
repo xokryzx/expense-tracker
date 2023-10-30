efa2ea6f1017e56281a5cb2a31aed81209a10b49
xokryzx <xokryzx@gmail.com>
2023-10-31 03:42:56 +0900

    🩹 Fix data type from string to number

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -23,7 +23,7 @@ const ExpenseForm = ({ onCancel, onSaveExpenseData }) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
